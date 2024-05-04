b94600eca407e09eb82dd310b6dd82a98292cb32
sanju <sanju@investmates.io>
2024-05-05 02:17:41 +0530

    Lamda Function To S3 Bucket in Progress

--- cdk-workshop/lambda/hello.js
+++ cdk-workshop/lambda/hello.js
@@ -1,5 +1,23 @@
+import { promises as fsPromises } from 'fs';
+import * as AWS from 'aws-sdk';
+
 exports.handler = async function(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
+    const bucketName = process.env.bucketName || '';
+    const objectKey = 'output-file.txt';
+    const filePath = `/tmp/${objectKey}`;
+  
+    await fsPromises.writeFile(filePath, 'Contents from Lambda to local file');
+    const fileContents = await fsPromises.readFile(filePath);
+  
+    const s3 = new AWS.S3();
+    const params = {
+      Bucket: bucketName,
+      Key: objectKey,
+      Body: fileContents,
+    };
+    await s3.upload(params).promise();
+
     return {
       statusCode: 200,
       headers: { "Content-Type": "text/plain" },
--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,17 +1,29 @@
-import {  Stack, StackProps,App } from 'aws-cdk-lib';
+import {  Stack, StackProps,App,aws_s3, RemovalPolicy } from 'aws-cdk-lib';
 import { Runtime,Code, Function } from 'aws-cdk-lib/aws-lambda';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
+    const bucketId = "23bfe85n93n93n2345"
+    const bucket = new aws_s3.Bucket(this, 'S3Bucket', {
+      bucketName: `aws-lambda-s3-${bucketId}`,
+      removalPolicy: RemovalPolicy.RETAIN,
+    }
+  )
+
     const hello = new Function(this, 'HelloHandler', {
       runtime: Runtime.NODEJS_20_X,
       code: Code.fromAsset('lambda'),
-      handler: 'hello.handler'
+      handler: 'hello.handler',
+      environment : {
+        bucketName: bucket.bucketName,
+      }
     });
     new LambdaRestApi(this, 'Endpoint', {
       handler: hello
     });
+    
+    bucket.grantReadWrite(hello)
   }
 }
